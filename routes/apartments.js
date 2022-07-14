b4ef155fc641cd04bc931344693d3712ba66bbf5
omiras <oscar.miras@gmail.com>
2022-07-14 20:48:59 +0200

    updated

--- routes/apartments.js
+++ routes/apartments.js
@@ -21,7 +21,9 @@ router.get("/search", apartmentsController.getFilteredApartments);
 /* La función checkAuth se va a encargar de comprobar si el usuario esta autentificado como administrador para poder acceder al controlador, y se debe colocar en cada una de las rutas que requieran autentificación */
 router.get('/new-apartment', checkAuth, apartmentsController.getAddApartment);
 router.post('/new-apartment', checkAuth, apartmentsController.postAddApartment);
+
 router.get('/:id/edit', checkAuth, apartmentsController.getEditApartment);
+router.post('/:id/edit', checkAuth, apartmentsController.postEditApartment);
 
 // ruta detalle del apartamento
 router.get('/:id', apartmentsController.getApartmentDetail)
