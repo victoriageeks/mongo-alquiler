d6c84bbe3df36c2bc58c1e2c938d49b78591f2d4
omiras <oscar.miras@gmail.com>
2022-07-14 20:40:28 +0200

    editar apartamento

--- controllers/apartment.js
+++ controllers/apartment.js
@@ -1,7 +1,9 @@
 const Apartment = require('../models/apartment');
 
 const getAddApartment = (req, res) => {
-    res.render('add-apartment');
+    res.render('add-apartment', {
+        document: {}
+    });
 }
 
 const getAllApartments = async (req, res) => {
@@ -39,13 +41,13 @@ const getEditApartment = async (req, res) => {
     // 1. Recuperar el apartamento que se quiere editar
     const { id } = req.params;
     // 2. Ir a MongoDB y traerte el documento identificado con ese ID
-    // const document = await modelApartments.getApartmentById(id);
+    const document = await Apartment.findById(id);
 
     // // 3. Renderizar el formulario pero ahora se deben rellenar los campos con los valores del documento
-    // // TODO: En add-apartment.ejs comprobar si la variable document tiene valor. En tal caso, rellenar cada uno de los campos del formularo con los datos del objeto. 
-    // res.render("add-apartment", {
-    //     document
-    // })
+
+    res.render("add-apartment", {
+        document
+    });
 }
 
 const getApartmentDetail = async (req, res) => {
@@ -76,11 +78,29 @@ const getFilteredApartments = async (req, res) => {
     });
 }
 
+const postEditApartment = async (req, res) => {
+    // 1. Recuperar los campos del formulario
+    const { id } = req.params;
+    const { title, meters, photo, price } = req.body;
+
+    // 2. Actualizar el apartamento en la base de datos
+    await Apartment.findByIdAndUpdate(id, {
+        title,
+        meters,
+        photo,
+        price
+    });
+
+    // 3. Redireccionar al apartamento editado
+    res.redirect(`/apartment/${id}`);
+}
+
 module.exports = {
     getAddApartment,
     getAllApartments,
     postAddApartment,
     getEditApartment,
     getApartmentDetail,
-    getFilteredApartments
+    getFilteredApartments,
+    postEditApartment
 }
